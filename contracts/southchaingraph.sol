ce380354bb613fd26f7a8210a1b179f09a2b1989
RyanPark88 <53798531+RyanPark88@users.noreply.github.com>
2020-08-05 01:52:00 -0400

    Update and rename Gravity.sol to southchaingraph.sol

--- contracts/southchaingraph.sol
+++ contracts/southchaingraph.sol
@@ -1,53 +1,53 @@
-pragma solidity ^0.4.0;
+pragma solidity >=0.4.22 <0.7.0;
 
-contract GravatarRegistry {
-  event NewGravatar(uint id, address owner, string displayName, string imageUrl);
+contract NetworkRegistry {
+  event NewNetwork(uint id, address owner, string displayName, string imageUrl);
   event UpdatedGravatar(uint id, address owner, string displayName, string imageUrl);
 
-  struct Gravatar {
+  struct Network {
     address owner;
     string displayName;
     string imageUrl;
   }
 
-  Gravatar[] public gravatars;
+  Network[] public networks;
 
   mapping (uint => address) public gravatarToOwner;
   mapping (address => uint) public ownerToGravatar;
 
   function createGravatar(string _displayName, string _imageUrl) public {
     require(ownerToGravatar[msg.sender] == 0);
-    uint id = gravatars.push(Gravatar(msg.sender, _displayName, _imageUrl)) - 1;
+    uint id = networks.push(Network(msg.sender, _displayName, _imageUrl)) - 1;
 
     gravatarToOwner[id] = msg.sender;
     ownerToGravatar[msg.sender] = id;
 
-    emit NewGravatar(id, msg.sender, _displayName, _imageUrl);
+    emit NewNetwork(id, msg.sender, _displayName, _imageUrl);
   }
 
   function getGravatar(address owner) public view returns (string, string) {
     uint id = ownerToGravatar[owner];
-    return (gravatars[id].displayName, gravatars[id].imageUrl);
+    return (networks[id].displayName, networks[id].imageUrl);
   }
 
   function updateGravatarName(string _displayName) public {
     require(ownerToGravatar[msg.sender] != 0);
-    require(msg.sender == gravatars[ownerToGravatar[msg.sender]].owner);
+    require(msg.sender == networks[ownerToGravatar[msg.sender]].owner);
 
     uint id = ownerToGravatar[msg.sender];
 
-    gravatars[id].displayName = _displayName;
-    emit UpdatedGravatar(id, msg.sender, _displayName, gravatars[id].imageUrl);
+    networks[id].displayName = _displayName;
+    emit UpdatedGravatar(id, msg.sender, _displayName, networks[id].imageUrl);
   }
 
-  function updateGravatarImage(string _imageUrl) public {
+  function updateNetworkImage(string _imageUrl) public {
     require(ownerToGravatar[msg.sender] != 0);
-    require(msg.sender == gravatars[ownerToGravatar[msg.sender]].owner);
+    require(msg.sender == networks[ownerToGravatar[msg.sender]].owner);
 
     uint id = ownerToGravatar[msg.sender];
 
-    gravatars[id].imageUrl =  _imageUrl;
-    emit UpdatedGravatar(id, msg.sender, gravatars[id].displayName, _imageUrl);
+    networks[id].imageUrl =  _imageUrl;
+    emit UpdatedGravatar(id, msg.sender, networks[id].displayName, _imageUrl);
   }
 
   // the gravatar at position 0 of gravatars[]
@@ -56,8 +56,9 @@ contract GravatarRegistry {
   // that doesn't really exist
   // dani will invoke this function once when this contract is deployed
   // but then no more
-  function setMythicalGravatar() public {
+  function setNetwork() public {
     require(msg.sender == 0x8d3e809Fbd258083a5Ba004a527159Da535c8abA);
-    gravatars.push(Gravatar(0x0, " ", " "));
+    networks.push(Network(0x0, " ", " "));
   }
 }
+
